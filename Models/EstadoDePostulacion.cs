7540a2c77de1a88fa5634e59b07c5f99bde37edf
Fabiancastillo16 <castillorojas8@gmail.com>
2016-12-17 19:55:37 -0300

    Update clases
    modificacion entidades EstadoDePostulacion y Paper

--- Models/EstadoDePostulacion.cs
+++ Models/EstadoDePostulacion.cs
@@ -4,12 +4,14 @@ namespace Publicaciones.Models{
     ///Atributos
         ///Tipo: Nombre de los estados por los que pasa el paper
         ///Fecha:Fecha en donde es de ese tipo
+        ///revista: revista a la cual esta asociada la postulacion
     ///<summary>
     ///<returns></returns>
     public class EstadoDePostulacion{
 
         public string Tipo{set;get;}
         public string Fecha{set;get;}
+        public Revista revista{set;get;}
 
     }
 }
--- Models/Paper.cs
+++ Models/Paper.cs
@@ -1,24 +1,30 @@
+using System.Collections.Generic;
 namespace Publicaciones.Models{
+    
     ///<summary>
     ///Clase paper. Articulo antes de ser publicado
     ///Atributos
+        ///id: identificador del paper
         ///Titulo: Nombre del paper
         ///FechaInicio:Fecha que comenzo a escribir el Articulo
         ///FechaTermino:Fecha en que se termino el articulo
         ///Abstract: Resumen del articulo
         ///autorCorrespondiente: Autor correspondiente del articulo
         ///autorPrincipal: Autor principal del articulo
+        ///cantidadRechazos: cantidad de rechazos que tiene el paper
     ///<summary>
     ///<returns></returns>
     public class Paper{
 
+        public string id{set;get;}
         public string Titulo{set;get;}
         public string FechaInicio{set;get;}
         public string FechaTermino{set;get;}
         public string Abstract{set;get;}
         public Autor autorCorrespondiente{set;get;}
         public Autor autorPrincipal{set;get;}
-        List<Autor> listaAutores;
+        public List<int> listaAutor;
+        public int cantidadRechazos{set;get;}
 
     }
 }
