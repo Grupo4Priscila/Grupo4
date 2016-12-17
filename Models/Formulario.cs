caef375028fa12be09bf16d3d720c149c0f1b7f8
Grupo4Priscila <pris.18p@gmail.com>
2016-12-17 00:28:12 -0300

    Primer Avance
    Added: Autor, Categoria, EstadoDePostulacion, Formulario, Grado,
    Honorario, Indice, Jerarquia, Paper, Publicacion, Revista
    Update: Persona
    No todo está completo

---
+++ Models/Formulario.cs
@@ -0,0 +1,14 @@
+namespace Publicaciones.Models{
+
+    public class Formulario{
+
+        public string Cpip{set;get;}
+        public string Doi{set;get;}
+        public string NombreIndice{set;get;}
+
+        public Autor AutorPrincipal{set;get;}
+
+
+        
+    }
+}
---
+++ Models/Grado.cs
@@ -0,0 +1,11 @@
+namespace Publicaciones.Models
+{
+    public class Grado
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Fecha { get; set; }
+
+    }
+
+}
