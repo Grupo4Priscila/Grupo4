c5ea66466ba396eba7889c71512dd3f09a6f0d3a
Grupo4Priscila <pris.18p@gmail.com>
2016-12-18 13:10:13 -0300

    fixed
    Update:Paper,Persona

--- Models/Paper.cs
+++ Models/Paper.cs
@@ -1,16 +1,36 @@
-using System.ComponentModel.DataAnnotations;
-
+using System.Collections.Generic;
 namespace Publicaciones.Models{
-
+    
+    ///<summary>
+    ///Clase paper. Articulo antes de ser publicado
+    ///Atributos
+        ///id: identificador del paper
+        ///Titulo: Nombre del paper
+        ///FechaInicio:Fecha que comenzo a escribir el Articulo
+        ///FechaTermino:Fecha en que se termino el articulo
+        ///Abstract: Resumen del articulo
+        ///autorCorrespondiente: Autor correspondiente del articulo
+        ///autorPrincipal: Autor principal del articulo
+        ///cantidadRechazos: cantidad de rechazos que tiene el paper
+        ///EstadoDePostulacion: lista de todos los estados de las postulaciones del paper
+    ///<summary>
+    ///<returns></returns>
     public class Paper{
 
-    [Key]
-        public string IdPaper{set;get;}
+        public string id{set;get;}
         public string Titulo{set;get;}
-        public Publicacion Publicacion{set;get;}
-//        public string FechaInicio{set;get;}
+        public Publicacion Publicacion{get;set;}
 
-//        public string FechaTermino{set;get;}
-//        public string Abstract{set;get;}
+ //       public string FechaInicio{set;get;}
+ //       public string FechaTermino{set;get;}
+ //       public string Abstract{set;get;}
+ //       public Autor autorCorrespondiente{set;get;}
+ //       public Autor autorPrincipal{set;get;}
+//        public List<Autor> listaAutor;
+ ///       public int cantidadRechazos{set;get;}
+        public List<EstadoDePostulacion> EstadoDePostulacion;
+
+
+
     }
 }
--- Models/Persona.cs
+++ Models/Persona.cs
@@ -2,20 +2,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Publicaciones.Models
 {
+    ///<summary>
+    ///Clase Persona
+    ///Atributos
+        ///Rut: Rut de la persona
+        ///Nombre: Nombre de la persona
+        ///Apellido: Apellido de la persona
+        //Email: Correo de la persona
+        ///FechaNacimiento: Fecha de nacimiento de la persona
+        ///Nacionalidad: Nacionalidad de la persona
+        ///Genero: genero de la persona, femenino o masculino
+    ///<summary>
+    ///<returns></returns>
+
     public class Persona
     {
         [Key]
         public string Rut { get; set; }
-
         public string Nombre { get; set; }
-
         public string Apellido { get; set; }
-
         public string Email{get; set; }
-
-        public string FechaNacimiento{get; set;}
- //       public string Nacionalidad{get; set;}
- //       public string Genero{get; set;}
+//        public string FechaNacimiento{get; set;}
+//        public string Nacionalidad{get; set;}
+//        public string Genero{get; set;}
         }
 
     }
